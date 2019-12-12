e9f05861e8cd3de7f9c5a429bb5330b1ead35453
Francesco Cardinale <testadicardi@gmail.com>
2019-12-12 16:00:45 +0100

    Add arguments, more tensorboard tracking.

--- train_all_transformer.py
+++ train_all_transformer.py
@@ -1,11 +1,49 @@
 import os
 import datetime
+import argparse
+from pathlib import Path
 
 import tensorflow as tf
 import numpy as np
 
 from losses import masked_crossentropy, masked_mean_squared_error
 from model.transformer_factory import new_everything
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mel_dir', dest='MEL_DIR', type=str)
+parser.add_argument('--n_samples', dest='MAX_SAMPLES', default=300, type=int)
+parser.add_argument('--dropout', dest='DROPOUT', default=0.1, type=float)
+parser.add_argument('--noisestd', dest='NOISE_STD', default=0.2, type=float)
+parser.add_argument('--mel', dest='MEL_CHANNELS', default=80, type=int)
+parser.add_argument('--epochs', dest='EPOCHS', default=10000, type=int)
+parser.add_argument('--batch_size', dest='BATCH_SIZE', default=16, type=int)
+parser.add_argument('--text_freq', dest='TEXT_FREQ', default=1000, type=int)
+parser.add_argument('--image_freq', dest='IMAGE_FREQ', default=1000, type=int)
+parser.add_argument('--learning_rate', dest='LEARNING_RATE', default=1e-4, type=float)
+args = parser.parse_args()
+
+mel_path = Path(args.MEL_DIR) / 'mels'
+metafile = Path(args.MEL_DIR) / 'train_metafile.txt'
+
+N_EPOCHS = args.EPOCHS
+N_SAMPLES = args.MAX_SAMPLES
+image_freq = args.IMAGE_FREQ
+text_freq = args.TEXT_FREQ
+num_layers = 4
+d_model = 256
+num_heads = 8
+dff = 512
+# TODO: differentiate
+# DFF_TEXT = 256
+dff_prenet = 256
+max_position_encoding = 1000
+dropout_rate = args.DROPOUT
+stop_prob_index = 2
+postnet_conv_filters = 256
+postnet_conv_layers = 5
+postnet_kernel_size = 5
+
+mel_channels = args.MEL_CHANNELS
 
 
 class TestTokenizer:
@@ -56,36 +94,17 @@ def get_norm_mel(mel_path, start_vec, end_vec):
     return norm_mel
 
 
-mel_path = '/Users/fcardina/forge/text-to-speech/data/LJSpeech-1.1/train_mels/mels'
-metafile = '/Users/fcardina/forge/text-to-speech/data/LJSpeech-1.1/train_mels/train_metafile.txt'
-
-N_SAMPLES = 1000
-num_layers = 4
-d_model = 256
-num_heads = 8
-dff = 512
-# TODO: differentiate
-# DFF_TEXT = 256
-dff_prenet = 256
-max_position_encoding = 1000
-dropout_rate = 0.1
-stop_prob_index = 2
-postnet_conv_filters = 256
-postnet_conv_layers = 5
-postnet_kernel_size = 5
-
-mel_channels = 80
 start_vec = np.ones((1, mel_channels)) * -3
 end_vec = np.ones((1, mel_channels))
 
 mel_text_stop_samples = []
 count = 0
 alphabet = set()
-with open(metafile, 'r', encoding='utf-8') as f:
+with open(str(metafile), 'r', encoding='utf-8') as f:
     for l in f.readlines():
         l_split = l.split('|')
         text = l_split[-1].strip().lower()
-        mel_file = os.path.join(mel_path, l_split[0] + '.npy')
+        mel_file = os.path.join(str(mel_path), l_split[0] + '.npy')
         norm_mel = get_norm_mel(mel_file, start_vec, end_vec)
         stop_probs = np.ones(norm_mel.shape[0], dtype=np.int64)
         stop_probs[-1] = 2
@@ -102,7 +121,8 @@ tokenized_mel_text_train_samples = [(mel, [start_tok] + tokenizer.encode(text) +
 mel_text_stop_gen = lambda: (pair for pair in tokenized_mel_text_train_samples[10:])
 mel_text_stop_dataset = tf.data.Dataset.from_generator(mel_text_stop_gen,
                                                        output_types=(tf.float64, tf.int64, tf.int64))
-mel_text_stop_dataset = mel_text_stop_dataset.shuffle(10000).padded_batch(4, padded_shapes=([-1, 80], [-1], [-1]))
+mel_text_stop_dataset = mel_text_stop_dataset.shuffle(10000).padded_batch(args.BATCH_SIZE,
+                                                                          padded_shapes=([-1, 80], [-1], [-1]))
 mel_text_stop_dataset = mel_text_stop_dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
 input_vocab_size = tokenizer.vocab_size
@@ -129,31 +149,37 @@ transformers = new_everything(start_vec=start_vec,
                               dropout_rate=dropout_rate)
 loss_coeffs = [1.0, 1.0, 1.0]
 transformers['mel_to_text'].compile(loss=masked_crossentropy,
-                                    optimizer=tf.keras.optimizers.Adam(1e-4, beta_1=0.9, beta_2=0.98, epsilon=1e-9))
+                                    optimizer=tf.keras.optimizers.Adam(args.LEARNING_RATE, beta_1=0.9, beta_2=0.98,
+                                                                       epsilon=1e-9))
 transformers['text_to_text'].compile(loss=masked_crossentropy,
-                                     optimizer=tf.keras.optimizers.Adam(1e-4, beta_1=0.9, beta_2=0.98, epsilon=1e-9))
+                                     optimizer=tf.keras.optimizers.Adam(args.LEARNING_RATE, beta_1=0.9, beta_2=0.98,
+                                                                        epsilon=1e-9))
 transformers['mel_to_mel'].compile(loss=[masked_mean_squared_error,
                                          masked_crossentropy,
                                          masked_mean_squared_error],
                                    loss_weights=loss_coeffs,
-                                   optimizer=tf.keras.optimizers.Adam(1e-4, beta_1=0.9, beta_2=0.98, epsilon=1e-9))
+                                   optimizer=tf.keras.optimizers.Adam(args.LEARNING_RATE, beta_1=0.9, beta_2=0.98,
+                                                                      epsilon=1e-9))
 transformers['text_to_mel'].compile(loss=[masked_mean_squared_error,
                                           masked_crossentropy,
                                           masked_mean_squared_error],
                                     loss_weights=loss_coeffs,
-                                    optimizer=tf.keras.optimizers.Adam(1e-4, beta_1=0.9, beta_2=0.98, epsilon=1e-9))
+                                    optimizer=tf.keras.optimizers.Adam(args.LEARNING_RATE, beta_1=0.9, beta_2=0.98,
+                                                                       epsilon=1e-9))
 batch_count = 0
 losses = {}
 summary_writers = {}
-image_freq = 20
 
+weights_paths = {}
 kinds = ['text_to_text', 'mel_to_mel', 'text_to_mel', 'mel_to_text']
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+os.makedirs(f'/tmp/weights/train/{current_time}/', exist_ok=False)
 for kind in kinds:
     summary_writers[kind] = tf.summary.create_file_writer(f'/tmp/summaries/train/{current_time}/{kind}')
+    weights_paths[kind] = f'/tmp/weights/train/{current_time}/{kind}_weights.hdf5'
     losses[kind] = []
 
-for epoch in range(10000):
+for epoch in range(N_EPOCHS):
     for (batch, (mel, text, stop)) in enumerate(mel_text_stop_dataset):
         output = {}
         output['text_to_text'] = transformers['text_to_text'].train_step(text, text)
@@ -166,6 +192,7 @@ for epoch in range(10000):
             for kind in kinds:
                 with summary_writers[kind].as_default():
                     plot_attention(output[kind], step=transformers[kind].optimizer.iterations)
+                transformers[kind].save_weights(weights_paths[kind])
         
         print(f'\nbatch {batch_count}')
         for kind in kinds:
@@ -173,25 +200,27 @@ for epoch in range(10000):
                 tf.summary.scalar('loss', output[kind]['loss'], step=transformers[kind].optimizer.iterations)
             print(f'{kind} mean loss: {sum(losses[kind]) / len(losses[kind])}')
         
-        if batch_count % 200 == 199:
-            print('\n>>> Training samples')
+        if batch_count % text_freq == 0:
+            pred = {}
+            test_val = {}
             for i in range(10, 13):
-                test_mel = mel_text_stop_samples[i][0]
-                test_sentence = tokenizer.encode(mel_text_stop_samples[i][1])
-                pred = transformers['mel_to_text'].predict(test_mel)
-                pred = tokenizer.decode(pred['output'].numpy())
-                text_pred = transformers['text_to_text'].predict(test_sentence)
-                text_pred = tokenizer.decode(text_pred['output'])
-                print('(target) {}\n(mel to text) {}\n(text to text) {}\n'.format(
-                    mel_text_stop_samples[i][1], pred, text_pred))
-            print('\n>>> Validation samples')
+                test_val['mel_to_text'] = mel_text_stop_samples[i][0]
+                test_val['text_to_text'] = tokenizer.encode(mel_text_stop_samples[i][1])
+                for kind in ['mel_to_text', 'text_to_text']:
+                    pred[kind] = transformers[kind].predict(test_val[kind])
+                    pred[kind] = tokenizer.decode(pred[kind]['output'])
+                    with summary_writers[kind].as_default():
+                        tf.summary.text(f'{kind} from training', pred[kind],
+                                        step=transformers[kind].optimizer.iterations,
+                                        description=f'Target: {test_val["text_to_text"]}')
             for i in range(3):
-                test_mel = mel_text_stop_samples[i][0]
-                test_sentence = tokenizer.encode(mel_text_stop_samples[i][1])
-                pred = transformers['mel_to_text'].predict(test_mel)
-                pred = tokenizer.decode(pred['output'].numpy())
-                text_pred = transformers['text_to_text'].predict(test_sentence)
-                text_pred = tokenizer.decode(text_pred['output'])
-                print('(target) {}\n(mel to text) {}\n(text to text) {}\n'.format(
-                    mel_text_stop_samples[i][1], pred, text_pred))
+                test_val['mel_to_text'] = mel_text_stop_samples[i][0]
+                test_val['text_to_text'] = tokenizer.encode(mel_text_stop_samples[i][1])
+                for kind in ['mel_to_text', 'text_to_text']:
+                    pred[kind] = transformers[kind].predict(test_val[kind])
+                    pred[kind] = tokenizer.decode(pred[kind]['output'])
+                    with summary_writers[kind].as_default():
+                        tf.summary.text(f'{kind} from training', pred[kind],
+                                        step=transformers[kind].optimizer.iterations,
+                                        description=f'Target: {test_val["text_to_text"]}')
         batch_count += 1
