76417199e75d23883de7b7acae7198ec077568f2
Francesco Cardinale <testadicardi@gmail.com>
2020-04-11 23:08:50 +0200

    Fix mel masking.

--- model/transformer_utils.py
+++ model/transformer_utils.py
@@ -64,7 +64,7 @@ def create_encoder_padding_mask(seq):
 
 
 def create_mel_padding_mask(seq):
-    seq = tf.reduce_sum(seq, axis=-1)
+    seq = tf.reduce_sum(tf.math.abs(seq), axis=-1)
     seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
     return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, y, x)
 
