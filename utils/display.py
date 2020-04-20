0af54e71f0f27ac657c2c5869baae126578ab4e7
cfrancesco <testadicardi@gmail.com>
2020-04-20 14:13:20 +0200

    Remove self-attention logs. Tidier attention plots.

--- utils/display.py
+++ utils/display.py
@@ -16,3 +16,25 @@ def buffer_mel(ms, sr):
     buf.seek(0)
     plt.close('all')
     return buf
+
+
+def tight_grid(images):
+    images = np.array(images)
+    if len(images.shape) != 3:
+        raise Exception
+    else:
+        n, y, x = images.shape
+    ratio = y / x
+    if ratio > 1:
+        ny = max(int(np.sqrt(n / ratio)), 1)
+        nx = int(n / ny)
+        nx += n - (nx * ny)
+        extra = nx * ny - n
+    else:
+        nx = max(int(np.sqrt(n * ratio)), 1)
+        ny = int(n / nx)
+        ny += n - (nx * ny)
+        extra = nx * ny - n
+    tot = np.append(images, np.zeros((extra, y, x)), axis=0)
+    img = np.block([[*tot[i * nx:(i + 1) * nx]] for i in range(ny)])
+    return img
--- utils/logging.py
+++ utils/logging.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import tensorflow as tf
 
 from utils.audio import invert_griffin_lim
-from utils.display import buffer_mel
+from utils.display import buffer_mel, tight_grid
 from preprocessing.utils import norm_tensor
 from utils.decorators import ignore_exception
 
@@ -75,10 +75,11 @@ class SummaryManager:
     @ignore_exception
     def display_attention_heads(self, outputs, tag=''):
         for k in outputs['attention_weights'].keys():
-            image_batch = norm_tensor(tf.expand_dims(outputs['attention_weights'][k][0, :, :, :], -1))
-            # dim 0 of image_batch is now number of heads
-            batch_plot_path = f'{tag}AttentionHeads/{k}'
-            self.add_image(str(batch_plot_path), image_batch)
+            if k.endswith('block2'):
+                image = tight_grid(norm_tensor(outputs['attention_weights'][k][0]))
+                # dim 0 of image_batch is now number of heads
+                batch_plot_path = f'{tag}AttentionHeads/{k}'
+                self.add_image(str(batch_plot_path), tf.expand_dims(tf.expand_dims(image, 0), -1))
     
     @ignore_exception
     def display_mel(self, mel, tag='', sr=22050):
