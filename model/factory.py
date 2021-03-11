1126467102cd6ff4675b51f66c84bd75f873af6d
cfrancesco <testadicardi@gmail.com>
2021-03-11 18:21:02 +0100

    Fix Path URL.

--- model/factory.py
+++ model/factory.py
@@ -9,9 +9,9 @@ from model.models import ForwardTransformer, Aligner
 def tts_ljspeech(version='v1') -> [tf.keras.models.Model, dict]:
     config_name = f'ljspeech_tts_config_{version}.yaml'
     weights_name = f'ljspeech_tts_weights_{version}.hdf5'
-    remote_dir = Path(f'https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/TransformerTTS/api_weights/')
-    config_path = tf.keras.utils.get_file(config_name, str(remote_dir / config_name))
-    weights_path = tf.keras.utils.get_file(weights_name, str(remote_dir / weights_name))
+    remote_dir = 'https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/TransformerTTS/api_weights/'
+    config_path = tf.keras.utils.get_file(config_name, remote_dir + config_name)
+    weights_path = tf.keras.utils.get_file(weights_name, remote_dir / weights_name)
     return tts_custom(config_path, weights_path)
 
 
