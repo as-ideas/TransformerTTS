02f33f2732bda95ffdbdce7a68033dda578f978d
cfrancesco <testadicardi@gmail.com>
2021-05-10 17:40:30 +0200

    Minor refactoring. Add direct model saving.

--- data/audio.py
+++ data/audio.py
@@ -12,9 +12,50 @@ import pyworld as pw
 
 
 class Audio():
-    def __init__(self, config: dict):
-        self.config = config
-        self.normalizer = getattr(sys.modules[__name__], config['normalizer'])()
+    def __init__(self,
+                 sampling_rate: int,
+                 n_fft: int,
+                 mel_channels: int,
+                 hop_length: int,
+                 win_length: int,
+                 f_min: int,
+                 f_max: int,
+                 normalizer: str,
+                 trim_long_silences: bool = None,
+                 trim_silence: bool = None,
+                 trim_silence_top_db: int = None,
+                 vad_window_length: int = None,
+                 vad_sample_rate: int = None,
+                 vad_moving_average_width: int = None,
+                 vad_max_silence_length: int = None,
+                 **kwargs):
+        
+        self.config = self._make_config(locals())
+        self.sampling_rate = sampling_rate
+        self.n_fft = n_fft
+        self.mel_channels = mel_channels
+        self.hop_length = hop_length
+        self.win_length = win_length
+        self.f_min = f_min
+        self.f_max = f_max
+        self.trim_long_silences = trim_long_silences
+        self.trim_silence = trim_silence
+        self.trim_silence_top_db = trim_silence_top_db
+        self.vad_window_length = vad_window_length
+        self.vad_sample_rate = vad_sample_rate
+        self.vad_moving_average_width = vad_moving_average_width
+        self.vad_max_silence_length = vad_max_silence_length
+        self.normalizer = getattr(sys.modules[__name__], normalizer)()
+    
+    def _make_config(self, locals) -> dict:
+        config = {}
+        for k in locals:
+            if (k != 'self') and (k != '__class__'):
+                if isinstance(locals[k], dict):
+                    config.update(locals[k])
+                else:
+                    config.update({k: locals[k]})
+        return dict(config)
     
     def _normalize(self, S):
         return self.normalizer.normalize(S)
@@ -25,18 +66,18 @@ class Audio():
     def _linear_to_mel(self, spectrogram):
         return librosa.feature.melspectrogram(
             S=spectrogram,
-            sr=self.config['sampling_rate'],
-            n_fft=self.config['n_fft'],
-            n_mels=self.config['mel_channels'],
-            fmin=self.config['f_min'],
-            fmax=self.config['f_max'])
+            sr=self.sampling_rate,
+            n_fft=self.n_fft,
+            n_mels=self.mel_channels,
+            fmin=self.f_min,
+            fmax=self.f_max)
     
     def _stft(self, y):
         return librosa.stft(
             y=y,
-            n_fft=self.config['n_fft'],
-            hop_length=self.config['hop_length'],
-            win_length=self.config['win_length'])
+            n_fft=self.n_fft,
+            hop_length=self.hop_length,
+            win_length=self.win_length)
     
     def mel_spectrogram(self, wav):
         """ This is what the model is trained to reproduce. """
@@ -51,15 +92,15 @@ class Audio():
         S = librosa.feature.inverse.mel_to_stft(
             amp_mel,
             power=1,
-            sr=self.config['sampling_rate'],
-            n_fft=self.config['n_fft'],
-            fmin=self.config['f_min'],
-            fmax=self.config['f_max'])
+            sr=self.sampling_rate,
+            n_fft=self.n_fft,
+            fmin=self.f_min,
+            fmax=self.f_max)
         wav = librosa.core.griffinlim(
             S,
             n_iter=n_iter,
-            hop_length=self.config['hop_length'],
-            win_length=self.config['win_length'])
+            hop_length=self.hop_length,
+            win_length=self.win_length)
         return wav
     
     def display_mel(self, mel, is_normal=True):
@@ -70,43 +111,43 @@ class Audio():
         ax = librosa.display.specshow(s_db,
                                       x_axis='time',
                                       y_axis='mel',
-                                      sr=self.config['sampling_rate'],
-                                      fmin=self.config['f_min'],
-                                      fmax=self.config['f_max'])
+                                      sr=self.sampling_rate,
+                                      fmin=self.f_min,
+                                      fmax=self.f_max)
         f.add_subplot(ax)
         return f
     
     def load_wav(self, wav_path):
-        y, sr = librosa.load(wav_path, sr=self.config['sampling_rate'])
-        if self.config['trim_long_silences']:
-            y = self.trim_long_silences(y)
-        if self.config['trim_silence']:
-            y = self.trim_silence(y)
-        if y.shape[0] % self.config['hop_length'] == 0:
+        y, sr = librosa.load(wav_path, sr=self.sampling_rate)
+        if self.trim_long_silences:
+            y = self.trim_audio_long_silences(y)
+        if self.trim_silence:
+            y = self.trim_audio_silence(y)
+        if y.shape[0] % self.hop_length == 0:
             y = np.pad(y, (0, 1))
         return y, sr
     
     def save_wav(self, y, wav_path):
-        sf.write(wav_path, data=y, samplerate=self.config['sampling_rate'])
+        sf.write(wav_path, data=y, samplerate=self.sampling_rate)
     
     def extract_pitch(self, y):
-        _f0, t = pw.dio(y.astype(np.float64), fs=self.config['sampling_rate'],
-                        frame_period=self.config['hop_length'] / self.config['sampling_rate'] * 1000)
-        f0 = pw.stonemask(y.astype(np.float64), _f0, t, fs=self.config['sampling_rate'])  # pitch refinement
+        _f0, t = pw.dio(y.astype(np.float64), fs=self.sampling_rate,
+                        frame_period=self.hop_length / self.sampling_rate * 1000)
+        f0 = pw.stonemask(y.astype(np.float64), _f0, t, fs=self.sampling_rate)  # pitch refinement
         
         return f0
     
-    def trim_silence(self, wav):
+    def trim_audio_silence(self, wav):
         trimmed = librosa.effects.trim(wav,
-                                       top_db=self.config['trim_silence_top_db'],
+                                       top_db=self.trim_silence_top_db,
                                        frame_length=256,
                                        hop_length=64)
         return trimmed[0]
     
     # from https://github.com/resemble-ai/Resemblyzer/blob/master/resemblyzer/audio.py
-    def trim_long_silences(self, wav):
+    def trim_audio_long_silences(self, wav):
         int16_max = (2 ** 15) - 1
-        samples_per_window = (self.config['vad_window_length'] * self.config['vad_sample_rate']) // 1000
+        samples_per_window = (self.vad_window_length * self.vad_sample_rate) // 1000
         wav = wav[:len(wav) - (len(wav) % samples_per_window)]
         pcm_wave = struct.pack("%dh" % len(wav), *(np.round(wav * int16_max)).astype(np.int16))
         voice_flags = []
@@ -114,7 +155,7 @@ class Audio():
         for window_start in range(0, len(wav), samples_per_window):
             window_end = window_start + samples_per_window
             voice_flags.append(vad.is_speech(pcm_wave[window_start * 2:window_end * 2],
-                                             sample_rate=self.config['vad_sample_rate']))
+                                             sample_rate=self.vad_sample_rate))
         voice_flags = np.array(voice_flags)
         
         def moving_average(array, width):
@@ -123,11 +164,15 @@ class Audio():
             ret[width:] = ret[width:] - ret[:-width]
             return ret[width - 1:] / width
         
-        audio_mask = moving_average(voice_flags, self.config['vad_moving_average_width'])
+        audio_mask = moving_average(voice_flags, self.vad_moving_average_width)
         audio_mask = np.round(audio_mask).astype(np.bool)
-        audio_mask[:] = binary_dilation(audio_mask[:], np.ones(self.config['vad_max_silence_length'] + 1))
+        audio_mask[:] = binary_dilation(audio_mask[:], np.ones(self.vad_max_silence_length + 1))
         audio_mask = np.repeat(audio_mask, samples_per_window)
         return wav[audio_mask]
+    
+    @classmethod
+    def from_config(cls, config: dict):
+        return cls(**config)
 
 
 class Normalizer:
--- train_tts.py
+++ train_tts.py
@@ -121,9 +121,6 @@ summary_manager = SummaryManager(model=model, log_dir=config.log_dir, config=con
 checkpoint = tf.train.Checkpoint(step=tf.Variable(1),
                                  optimizer=model.optimizer,
                                  net=model)
-manager = tf.train.CheckpointManager(checkpoint, config.weights_dir,
-                                     max_to_keep=config_dict['keep_n_weights'],
-                                     keep_checkpoint_every_n_hours=config_dict['keep_checkpoint_every_n_hours'])
 manager_training = tf.train.CheckpointManager(checkpoint, str(config.weights_dir / 'latest'),
                                               max_to_keep=1, checkpoint_name='latest')
 
@@ -184,9 +181,9 @@ for _ in t:
     
     if model.step % 1000 == 0:
         save_path = manager_training.save()
-    if model.step % config_dict['weights_save_frequency'] == 0:
-        save_path = manager.save()
-        t.display(f'checkpoint at step {model.step}: {save_path}', pos=len(config_dict['n_steps_avg_losses']) + 2)
+    if (model.step % config_dict['weights_save_frequency']) & (model.step >= config_dict['weights_save_starting_step']):
+        model.save_model(config.weights_dir / f'step_{model.step}')
+        t.display(f'checkpoint at step {model.step}: {config.weights_dir / f"step_{model.step}"}', pos=len(config_dict['n_steps_avg_losses']) + 2)
     
     if model.step % config_dict['validation_frequency'] == 0:
         t.display(f'Validating', pos=len(config_dict['n_steps_avg_losses']) + 3)
