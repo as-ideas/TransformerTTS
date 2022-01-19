079742f63628e7dd16e8f83c2a4731b0e5e5d210
Tanuj Jain <tanuj.jain.10@gmail.com>
2022-01-19 18:39:02 +0100

    Update tokenizer test to pass with the latest tokenizer.

--- tests/test_char_tokenizer.py
+++ tests/test_char_tokenizer.py
@@ -12,12 +12,12 @@ class TestCharTokenizer(unittest.TestCase):
         tokenizer = Tokenizer(alphabet=list('ab c'))
         self.assertEqual(5, tokenizer.start_token_index)
         self.assertEqual(6, tokenizer.end_token_index)
-        self.assertEqual(7, tokenizer.vocab_size)
-        
-        seq = tokenizer('a b d')
-        self.assertEqual([5, 1, 3, 2, 3, 6], seq)
-        
-        seq = np.array([5, 1, 3, 2, 8, 6])
+        self.assertEqual(8, tokenizer.vocab_size)
+
+        seq = tokenizer('a b c')
+        self.assertEqual([5, 7, 2, 1, 7, 3, 1, 7, 4, 6], seq)
+
+        seq = np.array([5, 2, 1, 3, 6])
         seq = tf.convert_to_tensor(seq)
         text = tokenizer.decode(seq)
         self.assertEqual('>a b<', text)
